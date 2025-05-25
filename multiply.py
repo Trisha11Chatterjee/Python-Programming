bb7f310f80abca57000f3d3b483739f8999f6a3d
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2025-05-26 01:28:34 +0530

    Add files via upload

---
+++ multiply.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a=3;
+b=2;
+c= a*b;
+print (c);

65e9319866a992a7333a87a5d8415b7ba651c07b
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2025-05-26 01:25:24 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+test = 10 
+print (test)
+print (test)
+
+print ("Edureka")
+print ("Edureka")
